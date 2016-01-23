7f16f5abd07db75989baf8c9f22dc9ad85e9f5cb
Cody Poll <cjpoll@gmail.com>
2016-01-23 09:51:10 -0700

    Webpack indentation
    It's annoying to do a diff after fixing indentation without a commit.

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,45 @@ var path = require("path");
 var fs = require("fs");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./web/static/js/index.js"),
-  context: __dirname,
-  output: {
-    path: "./priv/static/js",
-    filename: "bundle.js"
-  },
-  resolve: {
-    modulesDirectories: [
-      './node_modules',
-      __dirname + "web/static/js"
-    ],
-    extensions: ['', '.js', '.jsx', '.scss'],
-    root: [path.resolve(__dirname, ".")]
-  },
-  eslint: {
-    configFile: path.resolve(__dirname,'./.eslintrc')
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel',
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        query: {
-          presets: ["es2015", "react"]
-        }
-      },
-      {
-        loader: 'url-loader',
-        query: {
-          limit: 8192
-        },
-        test: /\.(jpe?g|png|gif|svg|ttf|eot|woff|woff2)$/
-      },
-      {
-        loader: 'style-loader!css-loader!sass-loader',
-        test: /\.scss$/
-      }
-    ]
-  },
-  plugins: []
+	entry: path.resolve(__dirname, "./web/static/js/index.js"),
+	context: __dirname,
+	output: {
+		path: "./priv/static/js",
+		filename: "bundle.js"
+	},
+	resolve: {
+		modulesDirectories: [
+			'./node_modules',
+			__dirname + "web/static/js"
+		],
+		extensions: ['', '.js', '.jsx', '.scss'],
+		root: [path.resolve(__dirname, ".")]
+	},
+	eslint: {
+		configFile: path.resolve(__dirname,'./.eslintrc')
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel',
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				query: {
+					presets: ["es2015", "react"]
+				}
+			},
+			{
+				loader: 'url-loader',
+				query: {
+					limit: 8192
+				},
+				test: /\.(jpe?g|png|gif|svg|ttf|eot|woff|woff2)$/
+			},
+			{
+				loader: 'style-loader!css-loader!sass-loader',
+				test: /\.scss$/
+			}
+		]
+	},
+	plugins: []
 };

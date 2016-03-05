784a328b5f46bc3282a0452821b2f4a972b74665
CJPoll <CJPoll@gmail.com>
2016-03-04 23:27:00 -0700

    Min/Max stats enabled.

--- web/static/js/stores/characterGeneratorStore.js
+++ web/static/js/stores/characterGeneratorStore.js
@@ -4,27 +4,43 @@ import CharacterGeneratorActions from 'characterGeneratorActions';
 const CharacterGeneratorStore = Reflux.createStore({
 	listenables: CharacterGeneratorActions,
 
+	minStatValue: 6,
+	maxStatValue: 14,
+	startingPointCount: 60,
+
 	init() {
 		this.state = {
 			stats: {
-				'Power': 0,
-				'Social': 0,
-				'Skill': 0,
-				'Physical': 0,
-				'Resilience': 0,
-				'Mental': 0
+				'Power': this.minStatValue,
+				'Social': this.minStatValue,
+				'Skill': this.minStatValue,
+				'Physical': this.minStatValue,
+				'Resilience': this.minStatValue,
+				'Mental': this.minStatValue
 			}
 		};
 	},
 
-	onDecreaseStat(stat) {
-		this.state.stats[stat] -= 1;
-		this.trigger(this.state);
+	onIncreaseStat(stat) {
+		if (this.canRaise(stat)) {
+			this.state.stats[stat] += 1;
+			this.trigger(this.state);
+		}
 	},
 
-	onIncreaseStat(stat) {
-		this.state.stats[stat] += 1;
-		this.trigger(this.state);
+	onDecreaseStat(stat) {
+		if (this.canLower(stat)) {
+			this.state.stats[stat] -= 1;
+			this.trigger(this.state);
+		}
+	},
+
+	canRaise(stat) {
+		return this.state.stats[stat] < this.maxStatValue;
+	},
+
+	canLower(stat) {
+		return this.state.stats[stat] > this.minStatValue;
 	}
 });
 

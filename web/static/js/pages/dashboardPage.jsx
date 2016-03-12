10fe54fbb9d2bf2b5b64ceb4561a27ace31d0425
CJPoll <CJPoll@gmail.com>
2016-03-12 08:43:29 -0700

    Start ability list

--- web/static/js/pages/dashboardPage.jsx
+++ web/static/js/pages/dashboardPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import CompositeStatValue from 'compositeStatValue';
+import AbilityList from 'abilityList';
 
 const DashboardPage = React.createClass({
 	render() {
 		return (
 			<div>
 				<h2>Dashboard</h2>
-				<CompositeStatValue statName="Power" statValue={'Over 9000!'} />
+				<AbilityList label="Strengths" listItems={['Strengh', 'Constitution']} />
 			</div>
 		);
 	}

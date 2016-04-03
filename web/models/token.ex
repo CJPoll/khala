5d33ed766c32df3209a7a5c3625c9321f15b0af0
CJPoll <CJPoll@gmail.com>
2016-04-03 14:51:59 -0600

    Updated Token for Ecto 2

--- web/models/token.ex
+++ web/models/token.ex
@@ -11,8 +11,8 @@ defmodule Khala.Token do
     timestamps
   end
 
-  @required_fields ~w(token user_id expired)
-  @optional_fields ~w()
+  @all_fields [:token, :user_id, :expired]
+  @required_fields [:token, :user_id, :expired]
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -20,9 +20,10 @@ defmodule Khala.Token do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ %{}) do
     model
-    |> cast(params, @required_fields, @optional_fields)
+    |> cast(params, @all_fields)
+    |> validate_required(@required_fields)
   end
 
   def user_for(token_uuid) do

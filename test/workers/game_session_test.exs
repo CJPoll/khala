f2129e567491ea6dfb8ef7f32f2d6b9ffce1207e
CJPoll <CJPoll@gmail.com>
2016-05-27 17:36:00 -0600

    Allow player to choose character in GameSession

--- test/workers/game_session_test.exs
+++ test/workers/game_session_test.exs
@@ -4,12 +4,20 @@ defmodule Khala.GameSession.Test do
 
   setup do
     user = test_user("cjpoll@khala.com")
+    campaign = test_campaign("Monteporte", user)
+    character = "Jim Hold"
+                |> test_character(user, campaign)
+                |> Khala.Database.Character.preload(:user)
+
+    user = Khala.Database.User.preload(user, :characters)
 
     {:ok, session} = Khala.GameSession.start_session
 
     state = %{
-      user: user,
-      session: session
+      campaign: campaign,
+      character: character,
+      session: session,
+      user: user
     }
 
     {:ok, state}
@@ -24,5 +32,13 @@ defmodule Khala.GameSession.Test do
     assert [user] == Khala.GameSession.players(session)
   end
 
-  test "it allows a player to select a character"
+  test "it allows a player to select a character", context do
+    user = context.user
+    session = context.session
+    character = context.character
+
+    :ok = Khala.GameSession.choose_character(session, user, character)
+
+    assert character == Khala.GameSession.character_for_player(session, user)
+  end
 end
--- web/workers/game_session.ex
+++ web/workers/game_session.ex
@@ -4,16 +4,13 @@ defmodule Khala.GameSession do
   @type t :: pid
 
   defmodule State do
-    defstruct players: []
+    defstruct players: [],
+    characters: %{}
   end
 
+  @spec start_session :: t
   def start_session do
     GenServer.start_link(__MODULE__, [])
-  end
-
-  def init(_args) do
-    state = %State{}
-    {:ok, state}
   end
 
   @spec add_player(t, %Khala.User{}) :: :ok
@@ -21,9 +18,39 @@ defmodule Khala.GameSession do
     GenServer.cast(session, {:add_player, player})
   end
 
+  @spec character_for_player(t, %Khala.User{}) :: %Khala.Character{}
+  def character_for_player(session, player) do
+    GenServer.call(session, {:character_for, player})
+  end
+
+  def choose_character(session, player, character) do
+    GenServer.call(session, {:choose_character, player, character})
+  end
+
   @spec players(t) :: [%Khala.User{}]
   def players(session) do
     GenServer.call(session, :players)
+  end
+
+  def init(_args) do
+    state = %State{}
+    {:ok, state}
+  end
+
+  def handle_call({:character_for, player}, _from, state) do
+    character = Map.get(state.characters, player.id)
+    {:reply, character, state}
+  end
+
+  def handle_call({:choose_character, player, character}, _from, state) do
+    if player_owns_character?(player, character) do
+      state = %{state |
+        characters: set_character(state.characters, player, character) }
+
+      {:reply, :ok, state}
+    else
+      {:reply, {:error, :mismatched_character}, state}
+    end
   end
 
   def handle_call(:players, _from, %{players: players} = state) do
@@ -36,4 +63,13 @@ defmodule Khala.GameSession do
     {:noreply, state}
   end
 
+  # Private functions
+
+  defp player_owns_character?(player, character) do
+    player.id == character.user.id
+  end
+
+  defp set_character(characters, player, character) do
+    Map.put(characters, player.id, character)
+  end
 end
